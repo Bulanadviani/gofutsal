cb0ceffb8a9681c292bf4efb5d27f4f88236c461
Bulanadviani <115436639+Bulanadviani@users.noreply.github.com>
2024-01-17 09:21:54 +0700

    Add files via upload

---
+++ gofutsal/goofutsal.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8111
+-- Generation Time: Jul 27, 2023 at 10:10 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gofutsal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table gofutsal.failed_jobs: #1932 - Table &#039;gofutsal.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table gofutsal.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gofutsal`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lapangans`
+--
+-- Error reading structure for table gofutsal.lapangans: #1932 - Table &#039;gofutsal.lapangans&#039; doesn&#039;t exist in engine
+-- Error reading data for table gofutsal.lapangans: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gofutsal`.`lapangans`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table gofutsal.migrations: #1932 - Table &#039;gofutsal.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table gofutsal.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gofutsal`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+-- Error reading structure for table gofutsal.password_reset_tokens: #1932 - Table &#039;gofutsal.password_reset_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table gofutsal.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gofutsal`.`password_reset_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table gofutsal.personal_access_tokens: #1932 - Table &#039;gofutsal.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table gofutsal.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gofutsal`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table gofutsal.users: #1932 - Table &#039;gofutsal.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table gofutsal.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gofutsal`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
