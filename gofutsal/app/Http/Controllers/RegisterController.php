db99b93c7706227986cdc37e9c5ff6bc7d8f25a3
Bulanadviani <advianibulanfitria@gmail.com>
2024-01-17 09:18:20 +0700

    first commit

---
+++ gofutsal/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index(){
+        return view('register');
+    }
+
+
+    public function register(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required',
+        ]);
+
+        User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password),
+            'level' => 0,
+        ]);
+
+        return redirect('/login');
+
+    }
+}
